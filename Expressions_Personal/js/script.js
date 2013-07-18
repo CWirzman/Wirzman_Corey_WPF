dfc813078e13e4265a8b6deb33d7e66960e5610d
Corey Wirzman <CWirzman@fullsail.edu>
2013-07-18 18:27:52 -0400

    I Added strings and  an Array of states that I travel through to get to Florida.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -2,3 +2,8 @@
 // Date: 7/18/2013
 // Assignment: Expressions_Personal
 
+var travelTime = "It takes myself about 14 hours to get to Jacksonville, Florida and I travel through 7 states.";
+var travelLength = "What is the average I spend through each state?";
+var states = ["Pennsylvania", "Maryland", "Virginia", "North Carolina", "South Carolina", "Gorgia", "Florida"];
+var total = 14/7 + " " + "hours in each state.";
+var travelAmount = "It takes roughly about";
