91d3e12f316951603d142c83e1ea00df44f5c466
Corey Wirzman <CWirzman@fullsail.edu>
2013-07-18 19:54:46 -0400

    Added some comments to explain what c and c ++ represents. Trying to figure out why console.log(x +  c ++) = 10 when it should be 9.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -2,8 +2,13 @@
 // Date: 7/18/2013
 // Assignment: Expressions_Wacky
 
+//This is how many cats I did have.
 var c = 2;
-
+//I found a stray and c ++ represents adding another cat.
 c ++;
+//How man kittens were born after stray repoduced with both cats.
+var x = 6;
 
-console.log(c);
+x + c ++;
+
+console.log(x + c ++);
