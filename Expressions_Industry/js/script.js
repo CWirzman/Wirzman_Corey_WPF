937d7b39d155a7771709df28267746f5c47f7eb6
Corey Wirzman <CWirzman@fullsail.edu>
2013-07-18 19:02:35 -0400

    Added my variable and a string to my Industry script

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -2,3 +2,9 @@
 // Date: 7/18/2013
 // Assignment: Expressions_Industry
 
+//A string to ask how many shovels are assembled all night
+var allNight = "How many shovels are assembled all night by a team(2) of operators?"
+var hours = 8;
+var shovelsHourly = 132
+var shovelsDamagedHourly = 7;
+console.log();
