f53417e21844b9f3c2baf486c279e66171c0a248
Corey Wirzman <CWirzman@fullsail.edu>
2013-07-18 19:26:54 -0400

    Changed from my initial assignment name from Personal to Wacky. This specifies that this is part of my Wacky Assignment. I also deleted the variables and numbers that were already inserted to start new.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,10 +1,4 @@
 // Name: Corey Wirzman
 // Date: 7/18/2013
-// Assignment: Expressions_Personal
+// Assignment: Expressions_Wacky
 
-
-var box = 330;
-var hourly = 0.5;
-var allNight = 8
-var result = 330 * 0.5 * 8;
-console.log(result);
