e448461718abbcf8d86ccc5d5cb52cbddbf13568
Corey Wirzman <CWirzman@fullsail.edu>
2013-07-18 19:20:33 -0400

    Added descriptions about the variables being used.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -4,17 +4,17 @@
 
 //A string to ask how many shovels are assembled all night
 var allNight = "How many shovels are assembled all night by a team(2) of operators?"
-
+//An explanation for the calculation being created
 explanation = "If there are 8 hours a day with 132 made an hour minus 7 scraped whats the total?";
-
+//hours in a work day
 hours = 8;
-
+//shovels made hourly
 shovelsHourly = 132
-
+//shovels damaged
 shovelsDamagedHourly = 7;
-
+//calculation of a work day
 var showTotal = "(132 -7) * 8 =";
-
+//A calculation to show how to get an amount of shovels created daily
 var total = (132 - 7) * 8;
 
 console.log(allNight);
