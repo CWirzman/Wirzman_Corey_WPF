198eb370d5f0bb1d2a8a5e3b77bb34a74cf1d4cc
Corey Wirzman <CWirzman@fullsail.edu>
2013-07-18 17:33:00 -0400

    This is my initial start for my Expressions Assignment: Expressions_Industry

---
+++ Expressions_Industry/js/script.js
@@ -0,0 +1,5 @@
+// Name: Corey Wirzman
+// Date: 7/18/2013
+// Assignment: Expressions_Personal
+
+var a = 3;
---
+++ Expressions_Personal/js/script.js
@@ -0,0 +1,10 @@
+// Name: Corey Wirzman
+// Date: 7/18/2013
+// Assignment: Expressions_Personal
+
+
+var box = 330;
+var hourly = 0.5;
+var allNight = 8
+var result = 330 * 0.5 * 8;
+console.log(result);
