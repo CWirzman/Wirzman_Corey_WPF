546881fe275e4a5b0243a68901d852cd218af81d
Corey Wirzman <CWirzman@fullsail.edu>
2013-07-18 18:50:00 -0400

    Added comments to all the variables to explain what they represent.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -2,10 +2,15 @@
 // Date: 7/18/2013
 // Assignment: Expressions_Personal
 
+//This explains how long it takes for me to get to Jacksonville, Florida and how many states I travel trough
 var travelTime = "It takes myself about 14 hours to get to Jacksonville, Florida. I travel through 7 states to reach my destination.";
+//Asking the average time spent in each state
 var travelLength = "What is the average time spent through each state?";
+//Array of states I travel through
 var states = ["Pennsylvania", "Maryland", "Virginia", "North Carolina", "South Carolina", "Gorgia", "Florida"];
+//The total is taking travelTime divided by the Array of states and adding a string to the end of thec total
 var total = 14/7 + " " + "hours in each state depending on speed and traffic.";
+//This is a string that starts the sentance before the total
 var travelAmount = "It takes roughly about";
 console.log(travelTime);
 console.log(travelLength);
