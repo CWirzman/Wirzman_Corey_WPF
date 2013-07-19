8b18acd600a130b5341213ff8415ba9135a4ebb5
Corey Wirzman <CWirzman@fullsail.edu>
2013-07-18 20:17:15 -0400

    I moved sentences next to the variables they represent. Then added a string to not just show an amount in console.log(). Lastly, I added spaces and the word cats to end the string + amount together to look like a sentence.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -10,10 +10,14 @@ c ++;
 
 //How man kittens were born after third added cat repoduced with both cats.
 var x = 6;
+
 //Taking x * c
-
 x + c;
-//showing what x * c will equal
 
+//showing what x * c will equal
 var amount = x + c;
-console.log(amount);
+
+//String to say what happened between all of my cats
+var cats = " After my finding a stray, my cats then mating with her and I now have";
+
+console.log( cats + " " + amount + " " + "cats.");
