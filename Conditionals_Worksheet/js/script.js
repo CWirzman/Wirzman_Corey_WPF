1a292c7b95b04978b9eca594877db822e702bff4
Corey Wirzman <CWirzman@fullsail.edu>
2013-07-24 14:07:46 -0400

    I changed the variables for all the students grades and then added if his total grade is less than the variable highestAmountEarn it will show his grade. If it is greater than or equal to highestAmountEarn than it will show a string stating he earned the highest grade.

--- Conditionals_Worksheet/js/script.js
+++ Conditionals_Worksheet/js/script.js
@@ -2,10 +2,20 @@
 // Corey Wirzman
 
 //Grades student got on four tests
-var gradeOne = 82;
-var gradeTwo = 96;
-var gradeThree = 87;
-var gradeFour = 90;
+var gradeOne = 99;
+var gradeTwo = 99;
+var gradeThree = 99.25;
+var gradeFour = 99;
 //Total of all four grades together 
-var total = (82 + 96 + 87 + 90)/4;
-console.log(total);
+var total = (99 + 99 + 99.25 + 99)/4;
+//Stating that 
+var highestAmountEarn = 100;
+
+if(total >= highestAmountEarn){
+// This statment will show if the total is less than 100
+	console.log("You have a" + " " + total + ", which means you have earned an" + " " + total +
+	 " " + "in the class.");
+}else{
+//This statement will show if the total is equal to highestAmountEarn	
+	console.log("You earned the highest grade amount possible for the class.");
+}
