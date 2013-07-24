019b450b0127741f07a455b40914a92eac60896d
Corey Wirzman <CWirzman@fullsail.edu>
2013-07-24 13:38:41 -0400

    I added the statement if 32 degrees Fahrenheit is greater than 0 degrees Celsius than the statement is true if not, then vise versa.

--- Conditionals_Worksheet/js/script.js
+++ Conditionals_Worksheet/js/script.js
@@ -3,3 +3,14 @@
 
 //convert temperatures to degrees Celsius to Fahrenheit
 
+var cels = 0;
+var fahren = 32;
+
+//the temperature is x degrees
+if (fahren < cels){
+	//if the statement is true
+	console.log("The temperature is 32 degrees Fahrenheit");
+}else{
+	//if the statement is 
+	console.log("The temperature is 0 degrees Celsius");
+}
