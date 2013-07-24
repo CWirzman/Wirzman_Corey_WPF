693b84c472c550c1c9c310e291aad2350b49cc33
Corey Wirzman <CWirzman@fullsail.edu>
2013-07-24 14:26:01 -0400

    I started example number 3. So i started over with new everything. Then I stated what variables the tires meant in psi. Then said if the amount of all 4 tires are over "x" then the tires pass. If they are less than "x" you need to get them checked.

--- Conditionals_Worksheet/js/script.js
+++ Conditionals_Worksheet/js/script.js
@@ -1,21 +1,15 @@
 //Conditionals Worksheet
 // Corey Wirzman
 
-//Grades student got on four tests
-var gradeOne = 99;
-var gradeTwo = 99;
-var gradeThree = 99.25;
-var gradeFour = 99;
-//Total of all four grades together 
-var total = (99 + 99 + 99.25 + 99)/4;
-//Stating that 
-var highestAmountEarn = 100;
+//Stating what the psi is for the front and back tires
+var frontTires = 40;
+var backTires = 38;
 
-if(total >= highestAmountEarn){
-// This statment will show if the total is less than 100
-	console.log("You have a" + " " + total + ", which means you have earned an" + " " + total +
-	 " " + "in the class.");
+//if the front and back tires are over "x" amount of psi
+if(frontTires && backTires > 36){
+//This statement is true	
+	console.log("The tires pass spec!")
 }else{
-//This statement will show if the total is equal to highestAmountEarn	
-	console.log("You earned the highest grade amount possible for the class.");
+//This statement is false	
+	console.log("Get your tires checked out!")
 }
