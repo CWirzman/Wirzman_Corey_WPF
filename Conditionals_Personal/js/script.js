afd8f19069d37688379e6bdb1b32b8788f92c604
Corey Wirzman <CWirzman@fullsail.edu>
2013-07-25 11:41:21 -0400

    I changed my averageMpg variable and the relation operator to check and see if this expression will work right. I also, swapped my strings in the console.log(s)

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -4,11 +4,11 @@
 //Date: July 25, 2013
 
 var estimatedMpg = 36;
-var averageMpg = 34;
+var averageMpg = 38;
 
 
-if(averageMpg <= estimatedMpg){
-	console.log("You will stop more frequently and spend more money on your trip down to Florida.");
-}else{
+if(averageMpg >= estimatedMpg){
 	console.log("You will spend less money on gas and arrive in Florida quicker!");
+}else{
+	console.log("You will stop more frequently and spend more money on your trip down to Florida.");
 }
