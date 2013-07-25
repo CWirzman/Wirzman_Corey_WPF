f37cecbb397a7f9080306ad9c19693c1b3844344
Corey Wirzman <CWirzman@fullsail.edu>
2013-07-25 11:49:23 -0400

    I added statements explaining what my expression stands for and for each console.log.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -3,12 +3,15 @@
 //Student Number: 0002343986
 //Date: July 25, 2013
 
+//Explains my average mpg and my estimated mpg
 var estimatedMpg = 36;
 var averageMpg = 38;
 
-
+//If average mpg is greater than or equal too estimated mpg
 if(averageMpg >= estimatedMpg){
+//I will spend less money and be in FL quicker
 	console.log("You will spend less money on gas and arrive in Florida quicker!");
 }else{
+//I will have to stop for gas more, spend more money, and arrive later in FL than planned.
 	console.log("You will stop more frequently and spend more money on your trip down to Florida.");
 }
