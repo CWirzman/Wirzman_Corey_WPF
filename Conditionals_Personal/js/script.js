54a662129e40e827ca4efb52dbf81a52c414ba0d
Corey Wirzman <CWirzman@fullsail.edu>
2013-07-25 11:52:11 -0400

    I sectioned the "if and else" expression to get ready to add a Ternary expression.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -7,11 +7,12 @@
 var estimatedMpg = 36;
 var averageMpg = 38;
 
-//If average mpg is greater than or equal too estimated mpg
+/*  //If average mpg is greater than or equal too estimated mpg
 if(averageMpg >= estimatedMpg){
 //I will spend less money and be in FL quicker
 	console.log("You will spend less money on gas and arrive in Florida quicker!");
 }else{
 //I will have to stop for gas more, spend more money, and arrive later in FL than planned.
 	console.log("You will stop more frequently and spend more money on your trip down to Florida.");
-}
+}  */
+
