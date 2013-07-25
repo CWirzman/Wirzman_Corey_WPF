aa2fee6c98e22285a10b8bd410f85f7821433615
Corey Wirzman <CWirzman@fullsail.edu>
2013-07-25 10:44:38 -0400

    I added another variable to say "I will have my break in my car" and then added an "else if" to my expression.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -4,12 +4,15 @@
 //Date: July 25, 2013
 
 //these are temperatures to specify if I am going to spend my break either outside, in my car, or inside.
-var breakOutsideTemp = 77;
-var breakInsideTemp = 70;
+var breakOutsideTemp = 82;
+var breakInsideTemp = 79;
+var breakInCarTemp = 88;
 
 
 if(breakOutsideTemp > breakInsideTemp){
-	console.log("I will spend my break outside.")
+	console.log("I will spend my break outside.");
+}else if(breakOutsideTemp < breakInCarTemp){
+	console.log("I will spend my break in my car");
 }else{
-	console.log("I will spend my break inside." )
+	console.log("I will spend my break inside." );
 }
