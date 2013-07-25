b25889e61648fcc19e951458c1661b9e53f11c70
Corey Wirzman <CWirzman@fullsail.edu>
2013-07-25 11:29:33 -0400

    I added variables. Also, in my "if and else" expression I added a Relation Operator to get an idea of how I plan to finish this expression.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -3,3 +3,12 @@
 //Student Number: 0002343986
 //Date: July 25, 2013
 
+var estimatedMpg = 36;
+var averageMpg = 34;
+
+
+if(averageMpg <= estimatedMpg){
+	console.log("");
+}else{
+	console.log("");
+}
