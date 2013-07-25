9649eef92a68a6867f1c3da1ed2b01d753e3f5ec
Corey Wirzman <CWirzman@fullsail.edu>
2013-07-25 11:55:36 -0400

    I added a Ternary expression to replace the old "if and else" expression that was sectioned out. They are both the same thing but one is "if and else" and the other is Ternary.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -16,3 +16,5 @@ if(averageMpg >= estimatedMpg){
 	console.log("You will stop more frequently and spend more money on your trip down to Florida.");
 }  */
 
+(averageMpg >= estimatedMpg) ? console.log("You will spend less money on gas and arrive in Florida quicker!")
+ : ("You will stop more frequently and spend more money on your trip down to Florida.");
