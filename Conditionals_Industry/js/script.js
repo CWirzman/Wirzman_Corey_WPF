621483fc7a78d129191e46e1f5f4ec82cd3e7853
Corey Wirzman <CWirzman@fullsail.edu>
2013-07-25 10:26:07 -0400

    I removed variables that I did not need. I then added an "if and else" expression explaining where I will spend my break to check if will work in the console.log.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -3,8 +3,13 @@
 //Student Number: 0002343986
 //Date: July 25, 2013
 
-
+//these are temperatures to specify if I am going to spend my break either outside, in my car, or inside.
 var breakOutsideTemp = 77;
-var breakInCarTemp = 92;
 var breakInsideTemp = 70;
-var temp = true;
+
+
+if(breakOutsideTemp > breakInsideTemp){
+	console.log("I will spend my break outside.")
+}else{
+	console.log("I will spend my break inside." )
+}
