e47b0b856e83816b96205909dfb401aedea82937
Corey Wirzman <CWirzman@fullsail.edu>
2013-08-01 13:14:13 -0400

    Statements were added to show what is going on in this function.

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -2,10 +2,12 @@
 // Name: Corey Wirzman
 // Date: August 1, 2013
 
-
+//A function for how many shovels are fordged, assembled, and scraped daily.
 var shovelsMade = function(fordged, assembled, scraped){
+//Funstion definition
 	var totalAmount =  (fordged + assembled) * 5 - scraped;
 	return totalAmount;
 }
 var compShovels = shovelsMade(1000, 882, 198);
+//Showing function total in the console.
 console.log("There are " + compShovels + " shovels made in a work week at Ames True Temper.");
