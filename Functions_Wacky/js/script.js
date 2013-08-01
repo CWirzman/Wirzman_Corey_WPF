9155d42f4f1c81c1bfc6d4a3331a091ffcc32509
Corey Wirzman <CWirzman@fullsail.edu>
2013-08-01 13:29:13 -0400

    I felt like changed the variable numbers to something different. Then I added strings into each console.log for equation in the if else statement.

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -3,14 +3,14 @@
 // Date: August 1, 2013
 
 
-var iPhone5 = 199.99;
-var htcOne = 149.99;
-var samsungGS4 = 199.00;
+var iPhone5 = 149.99;
+var htcOne = 129.99;
+var samsungGS4 = 199.99;
 
 if(iPhone5 > htcOne || htcOne < samsungGS4){
-console.log();
+console.log("I will get the HTC One.");
 }else if(iPhone5 >= samsungGS4){
-console.log();
+console.log("I will go for my second choice, the iPhone 5");
 }else{
-console.log();
+console.log("Well, I guess I am getting the Samsung Galaxy S4 *sad face expression*");
 }
