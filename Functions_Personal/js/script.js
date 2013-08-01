d261faee9e94718122b6a37a9d52c2066d187b6b
Corey Wirzman <CWirzman@fullsail.edu>
2013-08-01 15:18:00 -0400

    I called the statement and checked in the console.log to see if it worked. Then I added sentences for everything in the function statement.

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -3,10 +3,14 @@
 // Date: August 1, 2013
 
 
-
+//A function statment.
 function amountOfWood(){
+//An expression with arithmetic operators inside the function.
 	var twoByFours = 26;
 	var twoByTens = 21;
 	var totalPieces = twoByFours + twoByTens;
+//A Ternary to show the equation the in the console log.
 	(totalPieces < 42) ? console.log("You do not have enough pieces for framing the livingroom") : console.log("You have " + totalPieces + " pieces of 2x4s and 2x6s.");
 }
+//Calling the function statement.
+amountOfWood();
