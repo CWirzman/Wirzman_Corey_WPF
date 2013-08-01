828413b88f13b6ee42aec84262830f8967acdd13
Corey Wirzman <CWirzman@fullsail.edu>
2013-08-01 13:26:08 -0400

    My if else statement was created and a logical operator was added into the equation.

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -7,3 +7,10 @@ var iPhone5 = 199.99;
 var htcOne = 149.99;
 var samsungGS4 = 199.00;
 
+if(iPhone5 > htcOne || htcOne < samsungGS4){
+console.log();
+}else if(iPhone5 >= samsungGS4){
+console.log();
+}else{
+console.log();
+}
