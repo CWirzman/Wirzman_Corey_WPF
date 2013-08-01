a7c99fbbac6809fcc11bbcb1607a4b17e392d556
Corey Wirzman <CWirzman@fullsail.edu>
2013-08-01 15:07:48 -0400

    I unsectioned the "else is" statement and took out the idea of adding a ternary. I am unsure if it is possible to add a ternary with an "else if" statement. I then added sentences into the statement.

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -2,18 +2,17 @@
 // Name: Corey Wirzman
 // Date: August 1, 2013
 
-
+//Variables for types of phones
 var iPhone5 = 149.99;
 var htcOne = 129.99;
 var samsungGS4 = 199.99;
 
-/* if(iPhone5 > htcOne || htcOne < samsungGS4){
+
+//An if else equation with an "or" logical operator.
+ if(iPhone5 > htcOne || htcOne < samsungGS4){
 console.log("I will get the HTC One.");
-}else if(iPhone5 >= samsungGS4){
+}else if(iPhone5 < samsungGS4){
 console.log("I will go for my second choice, the iPhone 5");
 }else{
 console.log("Well, I guess I am getting the Samsung Galaxy S4 *sad face expression*");
 }
-*/
-
-(iPhone5 > htcOne || htcOne < samsungGS4) ? console.log() : console.log() : console.log(); 
